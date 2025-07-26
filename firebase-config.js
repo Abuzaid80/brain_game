662ef3e22afcaff2b39899bcd7d5f29e697b2694
Abuzaid80 <86767014+Abuzaid80@users.noreply.github.com>
2025-07-26 20:54:46 +0700

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,13 +1,5 @@
 // Firebase configuration
-const firebaseConfig = {
-    apiKey: "AIzaSyBXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
-    authDomain: "brain-quiz-xxxxx.firebaseapp.com",
-    databaseURL: "https://brain-quiz-xxxxx-default-rtdb.firebaseio.com",
-    projectId: "brain-quiz-xxxxx",
-    storageBucket: "brain-quiz-xxxxx.appspot.com",
-    messagingSenderId: "123456789012",
-    appId: "1:123456789012:web:abcdefghijklmnop"
-};
+
 
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
